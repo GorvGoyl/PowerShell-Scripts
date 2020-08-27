93882b7912a868c02959e58a66978d34319ccb96
jerrygoyal <1gouravgg@gmail.com>
2020-08-27 21:41:10 +0530

    added touch function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -37,8 +37,22 @@ function gitacp {
   git push
 }
 
-function touch {New-Item -ItemType File -Name ($args[0])}
+function touch
+{
+    $file = $args[0]
+    if($file -eq $null) {
+        throw "No filename supplied"
+    }
 
-
+    if(Test-Path $file)
+    {
+        throw "file already exists"
+    }
+    else
+    {
+        # echo $null > $file
+        New-Item -ItemType File -Name ($file)
+    }
+}
 
 
