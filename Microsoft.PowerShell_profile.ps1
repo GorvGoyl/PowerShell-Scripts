36d925895ef6184e1008d29e481af263b661f73f
jerrygoyal <1gouravgg@gmail.com>
2021-01-08 21:31:18 +0530

    added ohmyposh

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -22,47 +22,56 @@ if ($env:TERM_PROGRAM -eq "vscode") {
   Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
 }
 
-# open file explorer `ii .`
+# shortcut
+## open file explorer `ii .`
 
+# set colors and git status
+
+# method 1)
+# https://ohmyposh.dev/docs/powershell
+Set-PoshPrompt -Theme aliens
+
+
+# method 2)
 # colorize powershell and show git branch
 # Import-Module posh-git
-function Write-BranchName () {
-    try {
-        $branch = git rev-parse --abbrev-ref HEAD
+# function Write-BranchName () {
+#     try {
+#         $branch = git rev-parse --abbrev-ref HEAD
 
-        if ($branch -eq "HEAD") {
-            # we're probably in detached HEAD state, so print the SHA
-            $branch = git rev-parse --short HEAD
-            Write-Host " ($branch)" -ForegroundColor "red"
-        }
-        else {
-            # we're on an actual branch, so print it
-            Write-Host " ($branch)" -ForegroundColor "blue"
-        }
-    } catch {
-        # we'll end up here if we're in a newly initiated git repo
-        Write-Host " (no branches yet)" -ForegroundColor "yellow"
-    }
-}
+#         if ($branch -eq "HEAD") {
+#             # we're probably in detached HEAD state, so print the SHA
+#             $branch = git rev-parse --short HEAD
+#             Write-Host " ($branch)" -ForegroundColor "red"
+#         }
+#         else {
+#             # we're on an actual branch, so print it
+#             Write-Host " ($branch)" -ForegroundColor "blue"
+#         }
+#     } catch {
+#         # we'll end up here if we're in a newly initiated git repo
+#         Write-Host " (no branches yet)" -ForegroundColor "yellow"
+#     }
+# }
 
-function prompt {
-    $base = "PS "
-    $path = "$($executionContext.SessionState.Path.CurrentLocation)"
-    $userPrompt = "$('>' * ($nestedPromptLevel + 1)) "
+# function prompt {
+#     $base = "PS "
+#     $path = "$($executionContext.SessionState.Path.CurrentLocation)"
+#     $userPrompt = "$('>' * ($nestedPromptLevel + 1)) "
 
-    Write-Host "`n$base" -NoNewline
+#     Write-Host "`n$base" -NoNewline
 
-    if (Test-Path .git) {
-        Write-Host $path -NoNewline -ForegroundColor "green"
-        Write-BranchName
-    }
-    else {
-        # we're not in a repo so don't bother displaying branch name/sha
-        Write-Host $path -ForegroundColor "green"
-    }
+#     if (Test-Path .git) {
+#         Write-Host $path -NoNewline -ForegroundColor "green"
+#         Write-BranchName
+#     }
+#     else {
+#         # we're not in a repo so don't bother displaying branch name/sha
+#         Write-Host $path -ForegroundColor "green"
+#     }
 
-    return $userPrompt
-}
+#     return $userPrompt
+# }
 
 
 function gitacp {
